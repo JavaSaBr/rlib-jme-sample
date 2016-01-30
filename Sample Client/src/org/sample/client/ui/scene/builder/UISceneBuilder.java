3ab463599239d9b81bd579ef1a95c83335ab79fb
javasabr <javasabr@gmail.com>
2016-01-30 16:09:09 +0300

    обновление по клиенту

--- Sample Client/src/org/sample/client/ui/scene/builder/UISceneBuilder.java
+++ Sample Client/src/org/sample/client/ui/scene/builder/UISceneBuilder.java
@@ -9,9 +9,24 @@ import org.sample.client.ui.scene.UIScene;
  */
 public interface UISceneBuilder {
 
+	/**
+	 * Фаил для переопределения стандартных стилей.
+	 */
 	public static final String CSS_FILE_BASE = "/ui/css/base.css";
+
+	/**
+	 * Фаил для переопределения чтилей из внешних библиотек.
+	 */
 	public static final String CSS_FILE_EXTERNAL = "/ui/css/external.css";
+
+	/**
+	 * Фаил для описания стилей для своих id.
+	 */
 	public static final String CSS_FILE_CUSTOM_IDS = "/ui/css/custom_ids.css";
+
+	/**
+	 * Фаил для описания стилей своих классов.
+	 */
 	public static final String CSS_FILE_CUSTOM_CLASSES = "/ui/css/custom_classes.css";
 
 	/**
--- Sample Client/src/org/sample/client/ui/scene/builder/impl/LoginUISceneBuilder.java
+++ Sample Client/src/org/sample/client/ui/scene/builder/impl/LoginUISceneBuilder.java
@@ -1,55 +1,15 @@
 package org.sample.client.ui.scene.builder.impl;
 
-import javafx.collections.ObservableList;
-import javafx.scene.Group;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
 import org.sample.client.ui.scene.UIScene;
-import org.sample.client.ui.css.CSSFont;
-import org.sample.client.ui.scene.builder.UISceneBuilder;
 
-import static javafx.geometry.Pos.CENTER;
-import static javafx.scene.paint.Color.TRANSPARENT;
-import static org.sample.client.ui.css.CSSFont.FONTS;
-import static org.sample.client.util.GameUtil.getInputStream;
-import static rlib.ui.util.FXUtils.bindFixedSize;
+import javafx.scene.layout.StackPane;
 
 /**
  * Реализация конструктора сцены логина.
  * 
  * @author Ronn
  */
-public class LoginUISceneBuilder implements UISceneBuilder {
-
-	@Override
-	public UIScene build() {
-
-		for(final CSSFont font : FONTS) {
-			Font.loadFont(getInputStream(font.getPath()), font.getSize());
-		}
-
-        final Group root = new Group();
-
-		final UIScene scene = new UIScene(root);
-		scene.setFill(TRANSPARENT);
-
-		final ObservableList<String> stylesheets = scene.getStylesheets();
-		stylesheets.add(CSS_FILE_BASE);
-		stylesheets.add(CSS_FILE_EXTERNAL);
-		stylesheets.add(CSS_FILE_CUSTOM_IDS);
-		stylesheets.add(CSS_FILE_CUSTOM_CLASSES);
-
-		final StackPane container = scene.getContainer();
-		container.setAlignment(CENTER);
-
-		build(scene, container);
-
-		bindFixedSize(container, scene.widthProperty(), scene.heightProperty());
-
-		scene.notifyFinishBuild();
-
-		return scene;
-	}
+public class LoginUISceneBuilder extends AbstractUISceneBuilder {
 
 	/**
 	 * Построение UI авторизации.
